3786a1a1cff32e421559c1333b326351b0747cc5
Tran Quang Minh <moonlig73@gmail.com>
2024-06-18 15:02:38 +0700

    Exercise - update func sliding window module1-week2

--- module1/week2/max_sliding_window.py
+++ module1/week2/max_sliding_window.py
@@ -12,3 +12,22 @@ for i in range(full_cycles):
   max_values.append(current_max)
 
 print(max_values)
+
+
+# ----------C2-----------
+# vì 3 phần tử cuối kh cần duyệt nên - k , dễ nhìn thấy - k đi thì vẫn còn dư 1 phần tử ở cuối nên + 1
+start_indexs = list(range(0, len(num_list) - k + 1))
+# vì slicing phần end là [start:end - 1] nên end_index cần cộng lên 1
+end_indexs = list(range(k, len(num_list) + 1))
+
+for start_index, end_index in zip(start_indexs, end_indexs):
+  sub_list = num_list[start_index: end_index]
+  print(max(sub_list))
+
+# ----------C3-----------
+sub_list = list()
+for data in num_list:
+  sub_list.append(data)
+  if len(sub_list) == k:
+    print(max(sub_list))
+    sub_list.pop(0)
