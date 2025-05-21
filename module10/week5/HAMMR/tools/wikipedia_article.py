422d8409bf15b2095741988c140d35c6abbfd64a
Tran Quang Minh <moonlig73@gmail.com>
2025-05-21 19:22:12 +0700

    tools for Hammr

--- module10/week5/HAMMR/tools/wikipedia_article.py
+++ module10/week5/HAMMR/tools/wikipedia_article.py
@@ -1,7 +1,14 @@
-from langchain_community.tools.wikipedia.tool import WikipediaQueryRun
-from autogen_ext.tools.langchain import LangChainToolAdapter
+from langchain_community.utilities.wikipedia import WikipediaAPIWrapper
+from typing_extensions import Annotated
+from autogen_core.tools import FunctionTool
 
-def wikipedia_article(entity):
-  """Tool: Retrieve Wikipedia article for given entity."""
-  wikipedia_tool = LangChainToolAdapter(WikipediaQueryRun())
-  return wikipedia_tool.run(entity=entity)
+def wikipedia_article(entity: Annotated[str, "Entity or topic to retrieve Wikipedia article for"]) -> str:
+  """Tool: Retrieve Wikipedia article summary for given entity."""
+  wikipedia_api = WikipediaAPIWrapper()
+  return wikipedia_api.run(entity)
+
+
+wikipedia_article_tool = FunctionTool(
+    wikipedia_article,
+    description="Retrieve Wikipedia article summary for a given entity or topic."
+)
